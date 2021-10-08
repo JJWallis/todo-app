03e776d0a0902fd915eec713baa324b06d8ae085
JJWallis <josh.jamesonwallis@gmail.com>
2021-10-08 16:02:27 +0100

    Context ip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import styled, { ThemeProvider } from 'styled-components'
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
 import Wrapper from './components/Wrapper'
 import Header from './components/Header'
 import Main from './components/Main'
@@ -14,15 +14,24 @@ const darkTheme = {
    colorBg: 'hsl(235, 21%, 11%)',
 }
 
+export const Context: any = React.createContext({})
+
 const App: React.FC = () => {
+   const determineTheme = (light: boolean) => (light ? darkTheme : lightTheme)
+   // param as ThemeToggle state
+   // render returned value to theme prop below
+   // by updating local state here holds theme obj of usrs choice (toggles between them)
+
    return (
-      <ThemeProvider theme={lightTheme}>
-         <Wrapper body>
-            <Header />
-            <Main />
-            <Footer />
-         </Wrapper>
-      </ThemeProvider>
+      <Context.Provider value={determineTheme}>
+         <ThemeProvider theme={lightTheme}>
+            <Wrapper body>
+               <Header />
+               <Main />
+               <Footer />
+            </Wrapper>
+         </ThemeProvider>
+      </Context.Provider>
    )
 }
 
