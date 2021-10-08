2b7f0668e81ac47c3a126a57fd3dd7a9b1dd9e1d
JJWallis <josh.jamesonwallis@gmail.com>
2021-10-08 16:36:31 +0100

    Light theme updated

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,16 @@ import Footer from './components/Footer'
 const lightTheme = {
    fontFamily: 'Josefin Sans',
    colorBg: 'hsl(236, 33%, 92%)',
+   colorFg: 'hsl(0, 0%, 98%)',
+   fcSummary: 'hsl(233, 11%, 84%)',
+   fcTodo: 'hsl(235, 19%, 35%)',
+   fcTodoFtr: 'hsl(236, 9%, 61%)',
 }
 
 const darkTheme = {
    fontFamily: 'Josefin Sans',
    colorBg: 'hsl(235, 21%, 11%)',
+   colorFg: 'hsl(235, 24%, 19%)',
 }
 
 export const Context: any = React.createContext({})
--- style-guide.md
+++ style-guide.md
@@ -3,14 +3,6 @@
 -  Bright Blue: hsl(220, 98%, 61%)
 -  Check Background: linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)
 
-### Light Theme
-
--  Very Light Gray: hsl(0, 0%, 98%)
--  Very Light Grayish Blue: hsl(236, 33%, 92%)
--  Light Grayish Blue: hsl(233, 11%, 84%)
--  Dark Grayish Blue: hsl(236, 9%, 61%)
--  Very Dark Grayish Blue: hsl(235, 19%, 35%)
-
 ### Dark Theme
 
 -  Very Dark Blue: hsl(235, 21%, 11%)
