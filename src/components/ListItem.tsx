f9cf71b893e85205346651129ee7e7243f0e4bd4
JJWallis <josh.jamesonwallis@gmail.com>
2021-10-18 11:03:20 +0100

    Checkbox almost done

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,5 +1,4 @@
 import styled, { css } from 'styled-components'
-import Tick from '../assets/icon-check.svg'
 
 type ListItemProps = {
    todo?: boolean
@@ -23,32 +22,34 @@ const ListItem = styled.li<ListItemProps>`
          padding: 1rem 3.5rem;
          margin: 0;
          position: relative;
+         &:hover::before {
+            border-color: hsl(280, 87%, 65%);
+         }
          ${(props) =>
             !props.invisible &&
             css`
-          &::before {
-            content: '';
-            position: absolute;
-            top: 11px;
-            left: 11px;
-            width: 15px;
-            height: 15px;
-            border-radius: 50%;
-            padding: 1rem;
-            border: 1px solid ${(props) => props.theme.fcTodo};
-            z-index: 2;
-            transition: background-size 200ms ease-in;
-            ${(props) =>
-               props.completed &&
-               css`
-                  background-image: linear-gradient(
+               &::before {
+               content: '';
+               position: absolute;
+               top: 11px;
+               left: 11px;
+               width: 15px;
+               height: 15px;
+               border-radius: 50%;
+               padding: 1rem;
+               border: 0.01px solid ${(props) => props.theme.fcTodo};
+               z-index: 2;
+               transition: border-color 100ms ease-in;
+               ${(props) =>
+                  props.completed &&
+                  css`
+                     background-image: linear-gradient(
                         360deg,
                         hsl(280, 87%, 65%),
                         hsl(192, 100%, 67%)
-                     ),
-                     url(${Tick});
-               `}
-         `}
+                     );
+                  `}
+            `}
       `}
    ${(props) =>
       props.invisible
