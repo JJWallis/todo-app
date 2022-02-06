b2e642665a3427a52863498ef0e1cc3d274718ff
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-06 19:50:05 +0000

    TodoFooter ip

--- src/components/TodoFooter.tsx
+++ src/components/TodoFooter.tsx
@@ -15,18 +15,15 @@ const TodoFooter: React.FC = () => {
       { name: 'active', active: false },
       { name: 'completed', active: false },
    ])
-   // model as 1 obj + keys as name keys with boolean values
    const context = useContext(Context)
    const handleTodosVisibility = context.handleTodosVisibility
 
    const handleVisibleTodos = (val: string) => {
       const visible = [...visibleTodos]
-      // spread operator for copy of 1 obj as well
-      for (const btn of visible) btn.active = false
-      const desired: any = visible.find(
-         (btn: any) => btn.name === val.toLowerCase()
-         // access with bracket notation + below code in 1 line
-      )
+      visible.forEach((btn) => (btn.active = false))
+      const desired = visible.find(
+         (btn) => btn.name === val.toLowerCase()
+      ) as VisibleTodos
       desired.active = true
       setVisibleTodos(visible)
    }
