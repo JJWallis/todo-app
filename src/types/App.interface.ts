2cede0882017622a88a06f12b5bb6693dc6e7b44
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-06 17:13:51 +0000

    styled folder created & app type created

---
+++ src/types/App.interface.ts
@@ -0,0 +1,20 @@
+export interface AppState {
+   todos: Todo[]
+   determineTheme: (light: boolean) => void
+   addTodo: (todo: any) => void
+   handleRemoveTodo: (id: string) => void
+   handleClearCompleted: () => void
+   handleTodosVisibility: (
+      active?: string | undefined,
+      completed?: string | undefined
+   ) => void
+   handleCompletedTodo: (id: string) => void
+}
+
+export interface Todo {
+   id: string
+   key: string
+   value: string
+   isCompleted: boolean
+   invisible: boolean
+}
