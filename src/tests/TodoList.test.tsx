133e1d7184df413f08aee4c9b9cbe18fd9acfcb6
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-08 18:10:08 +0000

    TodoList test ip

--- src/tests/TodoList.test.tsx
+++ src/tests/TodoList.test.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 import TodoList from '../components/TodoList'
 import { render, screen } from './test-utils'
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
 
-test('initial', async () => {
+test('initial', () => {
    render(<TodoList />)
-   expect(screen.getByRole(''))
+   const imgs = screen.getAllByRole('img')
+   const summary = screen.getByText(/items left/i)
+   imgs.forEach((img) => expect(img).toHaveAttribute('src'))
+   expect(imgs).toHaveLength(2)
+   userEvent.click(imgs[imgs.length - 1])
+   expect(screen.getAllByRole('img')).toHaveLength(1)
+   expect(screen.getAllByRole('listitem')).toHaveLength(1)
+   screen.getByRole('')
 })
