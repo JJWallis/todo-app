f083d6e6fafab8fad147f4bfa412349bc5dbdef4
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-14 09:53:31 +0100

    first Cypress test in progress - correctly vising local host

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,11 @@
+/// <reference types="cypress" />
+
+// beforeEach(() => {
+//    cy.visit('http://localhost:3000')
+// })
+
 describe('My First Test', () => {
-   it('Does not do much!', () => {
-      expect(true).to.equal(false)
+   it('visits correct page', () => {
+      cy.visit('http://localhost:3000')
    })
 })
