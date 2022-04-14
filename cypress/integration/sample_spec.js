83f24d04789b638f69e7339e93ea03b68d7f4894
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-14 18:29:48 +0100

    reverted

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -8,6 +8,7 @@ describe('input field', () => {
    it('updates correctly on user input', () => {
       cy.contains('TODO')
       cy.get('.bmEWna').type('Hello').should('have.value', 'Hello')
+      //   this is a test commit
    })
    // commented added
 })
