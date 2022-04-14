7e35d3b531eeb3e82044c41ef74e86825006787a
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-14 10:00:07 +0100

    cypress visits local host before each test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,11 +1,11 @@
 /// <reference types="cypress" />
 
-// beforeEach(() => {
-//    cy.visit('http://localhost:3000')
-// })
+beforeEach(() => {
+   cy.visit('http://localhost:3000')
+})
 
 describe('My First Test', () => {
    it('visits correct page', () => {
-      cy.visit('http://localhost:3000')
+      const title = cy.contains('TODO')
    })
 })
